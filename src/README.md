0b39243730132654c39f647e4485bfe81b9da6d2
General-Meow <spl.ivalis@gmail.com>
2021-01-20 15:05:43 +0000

    feat: enable the reading of data from topic, added readme

---
+++ src/README.md
@@ -0,0 +1,14 @@
+# bigly-ws-prices
+
+Webservice that reads data from the company_price kafka topic and makes it available via a socket.io connection
+
+To setup:
+```shell script
+npm install
+```
+
+To run:
+
+```shell script
+node ./src/index.js
+```
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+const kafka = require('kafka-node')
 
+const kafkaHost = process.env.KAFKA_HOST || '192.168.1.81'
+const kafkaHostPort = process.env.KAFKA_HOST_PORT || '9092'
+const kafka_address = kafkaHost + ':' + kafkaHostPort
+const client = new kafka.KafkaClient({
+  kafkaHost: kafka_address
+})
+
+const consumer = new kafka.Consumer(client, [ {topic: "company_price", partition: 0} ], {autoCommit: false})
+
+consumer.on('message', function (message) {
+  console.log("got new message: ", message.value)
+})
 
 console.log('hello')
+
