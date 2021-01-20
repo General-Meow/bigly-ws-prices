99adae48ca381f8fa7fdda336f6a712f520ce250
General-Meow <spl.ivalis@gmail.com>
2021-01-20 15:30:12 +0000

    feat: add test framework to project

---
+++ test/test_index.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+
+describe("some overall thing like a class", () => {
+  describe("some method", () => {
+    it("should return true when blah", () => {
+      assert.equal(true, true)
+    })
+  })
+})
